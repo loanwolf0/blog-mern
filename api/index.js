730b5a35ffd78f1dbc52c9effb77d48139bce4bc
loanwolf0 <anurag12775@gmail.com>
2024-03-09 00:33:03 +0530

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,9 +1,19 @@
 const express = require('express')
-
+const mongoose = require('mongoose')
+require('dotenv').config()
 
 const app = express();
 
 
+mongoose.connect(process.env.MONGO)
+
+.then(() => {
+    console.log('mongoDB is connected');
+})
+
+.catch((err) => {
+    console.log({"ERROR" : err});
+})
 
 app.listen(3000, () => {
     console.log('App is runnig');
