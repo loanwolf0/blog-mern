35e167eeadb823f3ad345ecb358948dc53a719cf
loanwolf0 <anurag12775@gmail.com>
2024-03-09 19:18:39 +0530

    sign up created

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
   return (
     <Navbar className='border-b-2 '>
       <Link to='/' className='self-center whitespace-normal text-sm sm:text-xl font-semibold dark:text-white'>
-        <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 text-white rounded-lg'>ELV Drive</span>
+        <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white rounded-lg'>ELV Drive</span>
         Blog
       </Link>
 
